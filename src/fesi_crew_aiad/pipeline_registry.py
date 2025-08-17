4c4852a30fb9d8ca75372d613b2d312c2d608368
FatManSG <231988D@mymail.nyp.edu.sg>
2025-08-17 19:19:54 +0800

    Adding in our google drive data, creating pipeline for data ingestion

--- src/fesi_crew_aiad/pipeline_registry.py
+++ src/fesi_crew_aiad/pipeline_registry.py
@@ -1,16 +1,9 @@
 """Project pipelines."""
-from __future__ import annotations
-
-from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-
+import fesi_crew_aiad.pipelines.data_ingestion as data_ingestion
 
 def register_pipelines() -> dict[str, Pipeline]:
-    """Register the project's pipelines.
-
-    Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
-    """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    return {
+        "__default__": data_ingestion.create_pipeline(),
+        "data_ingestion": data_ingestion.create_pipeline(),
+    }
