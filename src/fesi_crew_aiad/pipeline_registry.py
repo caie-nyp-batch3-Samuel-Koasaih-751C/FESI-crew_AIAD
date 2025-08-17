46e136e544919d787c4f45b8cd5cfe1e3caaf69b
Marcus <marcusteomingxuan@gmail.com>
2025-08-17 20:07:49 +0800

    Added data preprocessing and data split pipelines

--- src/fesi_crew_aiad/pipeline_registry.py
+++ src/fesi_crew_aiad/pipeline_registry.py
@@ -1,9 +1,14 @@
 """Project pipelines."""
 from kedro.pipeline import Pipeline
 import fesi_crew_aiad.pipelines.data_ingestion as data_ingestion
+import fesi_crew_aiad.pipelines.data_preprocessing as data_preprocessing
+import fesi_crew_aiad.pipelines.data_split as data_split 
 
 def register_pipelines() -> dict[str, Pipeline]:
     return {
-        "__default__": data_ingestion.create_pipeline(),
+        "__default__": data_ingestion.create_pipeline()+ data_preprocessing.create_pipeline(),
         "data_ingestion": data_ingestion.create_pipeline(),
+        "data_preprocessing": data_preprocessing.create_pipeline(),
+        #put mask merge here
+        "data_split": data_split.create_pipeline(),
     }
